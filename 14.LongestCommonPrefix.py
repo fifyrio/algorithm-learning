68be6a5bf571b986dacf5190288161dcb2e53553
Will <fifyrio@qq.com>
2022-11-13 10:16:43 +0800

    Update 14.LongestCommonPrefix.py

--- 14.LongestCommonPrefix.py
+++ 14.LongestCommonPrefix.py
@@ -5,14 +5,17 @@ class Solution:
     # 遍历数组第一个元素+遍历数组
     # edge case: ["ab", "a"] -> "a"; ["cir", "car"] -> "c"
     # If n is the length of the array and m is the length of the shortest string, the worst case time complexity will be O(m × n)
+    # 空间复杂度: O(1)
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        if len(strs) == 1:
-            return strs[0]
-        res = ""
-        for i in range(len(strs[0])):
-            for s in strs:
-                # 不要漏了 第二个元素比第一个元素段的情况，使用数组index记得防止数组越界
-                if i == len(s) or s[i] != strs[0][i]:
-                    return res                    
-            res += strs[0][i]
-        return res
+        if not strs:
+            return ""
+
+        # 数组长度这种可以保存为常量，减少查询次数
+        length, count = len(strs[0]), len(strs)
+        for i in range(length):
+            for j in range(1, count):
+                # 不要漏了this case:第二个元素比第一个元素段的情况，使用数组index记得防止数组越界
+                if i == len(strs[j]) or strs[j][i] != strs[0][i]:
+                    # 不用单独一个变量来保存结果，直接用索引，:i表示[0,i)，<i的所有元素
+                    return strs[0][:i]
+        return strs[0]
