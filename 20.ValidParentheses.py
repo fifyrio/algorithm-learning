d715feffb5703eb048750feae93bf6c6f2a937ef
Will <fifyrio@qq.com>
2022-11-14 09:18:28 +0800

    Update 20.ValidParentheses.py

--- 20.ValidParentheses.py
+++ 20.ValidParentheses.py
@@ -3,22 +3,22 @@
 
 class Solution:
     # stack + mapper,
-    # time complex is O(n),
+    # time complex is O(n), n是字符串s的长度
     def isValid(self, s: str) -> bool:
+        # 剪枝，奇数长度的字符串肯定不是
+        if len(s) % 2 == 1:
+            return False      
         stack = []
         mapper = {
-            "(": ")",
-            "[": "]",
-            "{": "}",
+            ")": "(",
+            "]": "[",
+            "}": "{",
         }
         for i in s:
-            if i in mapper.keys():
-                stack.append(i)
-            else:
+            if i in mapper:
                 # For the case: ")(){}",首字母是右括号
-                if len(stack) == 0:
+                if len(stack) == 0 or mapper[i] != stack.pop():
                     return False
-                else:
-                    if mapper[stack.pop()] != i:
-                        return False
-        return len(stack) == 0
+            else:
+                stack.append(i)                
+        return len(stack) == 0   
